238a8fe757b5f6625838625d7e09ab9c0d312da7
Eko Suprapto Wibowo <swdev.bali@gmail.com>
2012-10-15 09:57:39 +0700

    init form : new species modul

---
+++ src/main/java/org/openforis/specieseditor/MyViewModel.java
@@ -0,0 +1,17 @@
+package org.openforis.specieseditor;
+
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.Init;
+
+public class MyViewModel {
+
+	@Init
+	public void init() {
+
+	}
+
+	@Command
+	public void cmd() {
+
+	}
+}
