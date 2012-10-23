ea3ed7caab8c6c7a797a269341bc7f9ce69fb10c
Eko Suprapto Wibowo <swdev.bali@gmail.com>
2012-10-23 10:38:01 +0700

    working with SelectorComposer, but still null for the inner call to other spring daos
    Signed-off-by: Eko Suprapto Wibowo <swdev.bali@gmail.com>

--- src/main/java/org/openforis/specieseditor/NewSpecies.java
+++ src/main/java/org/openforis/specieseditor/NewSpecies.java
@@ -5,21 +5,29 @@ import java.util.List;
 import javax.servlet.ServletContext;
 
 import org.openforis.collect.manager.SpeciesManager;
+import org.openforis.collect.persistence.TaxonDao;
+import org.openforis.collect.persistence.TaxonomyDao;
 import org.openforis.idm.model.TaxonOccurrence;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.Listen;
+import org.zkoss.zk.ui.select.annotation.VariableResolver;
 import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zk.ui.select.annotation.WireVariable;
+import org.zkoss.zk.ui.util.GenericAutowireComposer;
+import org.zkoss.zk.ui.util.GenericForwardComposer;
 import org.zkoss.zkplus.spring.SpringUtil;
 import org.zkoss.zul.Button;
 import org.zkoss.zul.Checkbox;
 import org.zkoss.zul.Messagebox;
 import org.zkoss.zul.Textbox;
+import org.zkoss.zul.Window;
 
 
-public class NewSpecies extends SelectorComposer<Component> {
+@VariableResolver(org.zkoss.zkplus.spring.DelegatingVariableResolver.class)
+public class NewSpecies extends SelectorComposer<Window>  {
 	@Wire
     private Button submitButton;    
    
@@ -35,17 +43,17 @@ public class NewSpecies extends SelectorComposer<Component> {
 	@Wire
 	private Textbox qualifier1Name;
 	
-	 
-    
-    @Listen("onClick = #okButton")
-    public void okButton(){
+	@WireVariable
+	private SpeciesManager speciesManager;
+	
+	@Listen("onClick=#okButton")
+    public void onOkButton(){
     	String taxonomy = taxonomyName.getText();
     	String species = speciesName.getText();
     	String vernacular = vernacularName.getText();
     	String qualifier1 = qualifier1Name.getText();
     	Messagebox.show("Saving of " + taxonomy + "," + species + ", " + vernacular + "," + qualifier1);
     	
-    	SpeciesManager speciesManager = (SpeciesManager) SpringUtil.getBean("speciesManager");
     	List<TaxonOccurrence> speciesList = speciesManager.findByScientificName(taxonomy, species, 1);
     	Messagebox.show(speciesList.size()+"");
     }
